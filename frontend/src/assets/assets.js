1eb5142fddf05a785cf5e88a0461b16badcbba6c
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2024-03-15 14:47:16 +0530

    Stage 3

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,21 +1,49 @@
-import Chicken1 from "./Chicken1.avif"
-import Chicken2 from "./Chicken2.avif"
-import Chicken3 from "./Chicken3.avif"
-import Chicken4 from "./Chicken4.avif"
-import Chicken5 from "./Chicken5.avif"
-import Chicken6 from "./Chicken6.avif"
-import Chicken7 from "./Chicken7.avif"
-import Chicken8 from "./Chicken8.avif"
-import Cake1 from "./Cake1.avif"
-import Cake2 from "./Cake2.avif"
-import Cake3 from "./Cake3.avif"
-import Cake4 from "./Cake4.avif"
-import Cake5 from "./Cake5.avif"
-import Cake6 from "./Cake6.avif"
-import Cake7 from "./Cake7.avif"
-import Cake8 from "./Cake8.avif"
+import Chicken1 from "./Chicken1.avif";
+import Chicken2 from "./Chicken2.avif";
+import Chicken3 from "./Chicken3.avif";
+import Chicken4 from "./Chicken4.avif";
+import Chicken5 from "./Chicken5.avif";
+import Chicken6 from "./Chicken6.avif";
+import Chicken7 from "./Chicken7.avif";
+import Chicken8 from "./Chicken8.avif";
+import Cake1 from "./Cake1.avif";
+import Cake2 from "./Cake2.avif";
+import Cake3 from "./Cake3.avif";
+import Cake4 from "./Cake4.avif";
+import Cake5 from "./Cake5.avif";
+import Cake6 from "./Cake6.avif";
+import Cake7 from "./Cake7.avif";
+import Cake8 from "./Cake8.avif";
+import Thali1 from "./Thali1.avif";
+import Thali2 from "./Thali2.avif";
+import Thali3 from "./Thali3.avif";
+import Thali4 from "./Thali4.avif";
+import Thali5 from "./Thali5.avif";
+import Thali6 from "./Thali6.avif";
+import Thali7 from "./Thali7.avif";
+import Thali8 from "./Thali8.avif";
+import Biryani1 from "./Biryani1.avif";
+import Biryani2 from "./Biryani2.avif";
+import Biryani3 from "./Biryani3.avif";
+import Biryani4 from "./Biryani4.avif";
+import Biryani5 from "./Biryani5.avif";
+import Biryani6 from "./Biryani6.avif";
+import Biryani7 from "./Biryani7.avif";
+import Biryani8 from "./Biryani8.avif";
+import Pizza1 from "./Pizza1.avif";
+import Pizza2 from "./Pizza2.avif";
+import Pizza3 from "./Pizza3.avif";
+import Pizza4 from "./Pizza4.avif";
+import Pizza5 from "./Pizza5.avif";
+import Pizza6 from "./Pizza6.avif";
+import Pizza7 from "./Pizza7.avif";
+import Pizza8 from "./Pizza8.avif";
+import Logo from "./Logo.png";
+import Logo2 from "./Logo2.png";
 
 export const assets = {
+    Logo,
+    Logo2,
     Chicken1,
     Chicken2,
     Chicken3,
@@ -32,4 +60,28 @@ export const assets = {
     Cake6,
     Cake7,
     Cake8,
-}
+    Thali1,
+    Thali2,
+    Thali3,
+    Thali4,
+    Thali5,
+    Thali6,
+    Thali7,
+    Thali8,
+    Pizza1,
+    Pizza2,
+    Pizza3,
+    Pizza4,
+    Pizza5,
+    Pizza6,
+    Pizza7,
+    Pizza8,
+    Biryani1,
+    Biryani2,
+    Biryani3,
+    Biryani4,
+    Biryani5,
+    Biryani6,
+    Biryani7,
+    Biryani8
+};
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -5,7 +5,8 @@ import { assets } from '../../assets/assets'
 const navbar = () => {
   return (
     <div className='navbar'>
-      <img src={assets.Chicken1} alt="" />
+      <img src={assets.Logo2} alt="" />
+      {/* <img src={assets.Chicken1} alt="" />
       <img src={assets.Chicken2} alt="" />
       <img src={assets.Chicken3} alt="" />
       <img src={assets.Chicken4} alt="" />
@@ -21,6 +22,30 @@ const navbar = () => {
       <img src={assets.Cake6} alt="" />
       <img src={assets.Cake7} alt="" />
       <img src={assets.Cake8} alt="" />
+      <img src={assets.Thali1} alt="" />
+      <img src={assets.Thali2} alt="" />
+      <img src={assets.Thali3} alt="" />
+      <img src={assets.Thali4} alt="" />
+      <img src={assets.Thali5} alt="" />
+      <img src={assets.Thali6} alt="" />
+      <img src={assets.Thali7} alt="" />
+      <img src={assets.Thali8} alt="" />
+      <img src={assets.Biryani1} alt="" />
+      <img src={assets.Biryani2} alt="" />
+      <img src={assets.Biryani3} alt="" />
+      <img src={assets.Biryani4} alt="" />
+      <img src={assets.Biryani5} alt="" />
+      <img src={assets.Biryani6} alt="" />
+      <img src={assets.Biryani7} alt="" />
+      <img src={assets.Biryani8} alt="" />
+      <img src={assets.Pizza1} alt="" />
+      <img src={assets.Pizza2} alt="" />
+      <img src={assets.Pizza3} alt="" />
+      <img src={assets.Pizza4} alt="" />
+      <img src={assets.Pizza5} alt="" />
+      <img src={assets.Pizza6} alt="" />
+      <img src={assets.Pizza7} alt="" />
+      <img src={assets.Pizza8} alt="" /> */}
     </div>
   )
 }
